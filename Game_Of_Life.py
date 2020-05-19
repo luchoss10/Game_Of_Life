a9750d8148eab2d4aff19f558ee20003b48af523
Luis Miguel Marmolejo Peña <lucncd@gmail.com>
2020-05-19 18:18:23 -0500

    Protocolo

--- Game_Of_Life.py
+++ Game_Of_Life.py
@@ -1,9 +1,19 @@
 import numpy as np
 
 def Make_World(i,j):
-    world = np.zeros(shape=(i,j))
+    world = np.zeros(shape=(i,j),dtype=int)
     return world
 
-world = Make_World(100,100)
+
+
+
+
+world = Make_World(10,10)
+
+print(world.dtype)
+print(world)
+
+
+
 
 
